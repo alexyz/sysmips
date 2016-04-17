6ebfc84a11a37df7a410d972f7e2eda3ad06312a
Alex S <alexslack56@gmail.com>
2016-04-17 09:31:25 +0100

    malta constants

--- src/sys/malta/Malta.java
+++ src/sys/malta/Malta.java
@@ -13,21 +13,29 @@ public class Malta implements Device {
 	/** the on board memory (128mb max) */
 	public static final int M_SDRAM = 0x0;
 	public static final int M_UNCACHED_EX_H = 0x100;
-	
 	public static final int M_PCI1 = 0x0800_0000;
 	// connected with value of [GT_PCI0IOLD]
 	public static final int M_PIIX4 = 0x1000_0000;
 	public static final int M_PCI2 = 0x1800_0000;
+	// system controller internal registers
 	public static final int M_GTBASE = 0x1be0_0000;
-	public static final int M_CBUS = 0x1c00_0000;
-	public static final int M_MONITORFLASH = 0x1e00_0000;
+	public static final int M_UNUSED = 0x1c00_0000;
+	// monitor flash/boot rom
+	public static final int M_FLASH1 = 0x1e00_0000;
 	public static final int M_RESERVED = 0x1e40_0000;
 	public static final int M_DEVICES = 0x1f00_0000;
-	public static final int M_DISPLAY = 0x1f00_0400;
-	public static final int M_SCSPEC1 = 0x1f10_0010;
-	public static final int M_BOOTROM = 0x1fc0_0000;
+	public static final int M_NMI = 0x1f00_0024;
+	public static final int M_NMIACK = 0x1f00_0104;
+	public static final int M_SWITCHES = 0x1f00_0200;
+	public static final int M_DISPLAYS = 0x1f00_0400;
+	public static final int M_CBUS_UART = 0x1f00_0900;
+	public static final int M_GPIO = 0x1f00_0a00;
+	public static final int M_I2C = 0x1f00_0b00;
+	public static final int M_GT2 = 0x1f10_0000;
+	// monitor flash/boot rom
+	public static final int M_FLASH2 = 0x1fc0_0000;
 	public static final int M_REVISION = 0x1fc0_0010;
-	public static final int M_SCSPEC2 = 0x1fd0_0010;
+	public static final int M_GT3 = 0x1fd0_0010;
 	
 	private static final Logger log = new Logger("Malta");
 	
@@ -42,7 +50,7 @@ public class Malta implements Device {
 		this.baseAddr = baseAddr;
 		this.p4 = new PIIX4(baseAddr + M_PIIX4);
 		this.gt = new GT(baseAddr + M_GTBASE);
-		this.display = new MaltaDisplay(baseAddr + M_DISPLAY);
+		this.display = new MaltaDisplay(baseAddr + M_DISPLAYS);
 	}
 	
 	public void setIrq (int irq) {
@@ -52,12 +60,10 @@ public class Malta implements Device {
 	@Override
 	public void init (Symbols sym) {
 		log.println("init malta at " + Integer.toHexString(baseAddr));
-		
+		sym.init(Malta.class, "M_", null, baseAddr, Integer.MAX_VALUE);
 		display.init(sym);
 		p4.init(sym);
 		gt.init(sym);
-		
-		sym.init(Malta.class, "M_", null, baseAddr, 4);
 	}
 	
 	@Override
